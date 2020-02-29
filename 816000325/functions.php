0dd182a6f9bf86496e36bdcb9bee3c47f93a3e6b
Ajay S <ajay_sieunarine@hotmail.com>
2020-02-29 18:04:20 -0400

    done, need to add validation checks on nis number field

--- 816000325/functions.php
+++ 816000325/functions.php
@@ -1,20 +1,75 @@
 <?php
 
-function initForm(){
+function initForm(&$nisNum, &$salaries, &$weeklyContributions, &$totalContributions, &$totSalary, &$totWeekly, &$totTotal){
     $nisNum = $_POST['nisNum'];
-    echo $nisNum;
-    initArray();
+    initSalaries($salaries, $totSalary);
+    initWeeklyContributions($weeklyContributions, $salaries, $totWeekly);
+    initTotalContributions($totalContributions, $salaries, $totTotal);
 }
 
-function initArray(){
+function initSalaries(&$salaries, &$totSalary){
     $salaries = array(
         $_POST['sal1'], $_POST['sal2'], $_POST['sal3'], $_POST['sal4'], $_POST['sal5']
     );
-
+    $totSalary = 0;
     for ($i = 0; $i < sizeof($salaries); $i++){
         if($salaries[$i] == null) $salaries[$i] = 0.00;
+        $totSalary += $salaries[$i];
     }
+}
 
-    echo implode(", ", $salaries);
-    return;
+function initWeeklyContributions(&$weeklyContributions, &$salaries, &$totWeekly){
+    $weeklyContributions = array();
+    $totWeekly = 0;
+    for ($i = 0; $i < sizeof($salaries); $i++){
+        $weeklyContributions[$i] = getWeeklyContribution($salaries[$i]);
+        $totWeekly += $weeklyContributions[$i];
+    }
+}
+function initTotalContributions(&$totalContributions, &$salaries, &$totTotal){
+    $totalContributions = array();
+    $totTotal = 0;
+    for ($i = 0; $i < sizeof($salaries); $i++){
+        $totalContributions[$i] = getTotalContribution($salaries[$i]);
+        $totTotal += $totalContributions[$i];
+    }
+}
+
+function getWeeklyContribution($val){
+    if($val > 200.00 && $val < 339.99) return 11.90;
+    if($val > 340.00 && $val < 449.99) return 17.40;
+    if($val > 450.00 && $val < 609.99) return 23.30;
+    if($val > 610.00 && $val < 759.99) return 30.10;
+    if($val > 760.00 && $val < 929.99) return 37.20;
+    if($val > 930.00 && $val < 1119.99) return 45.10;
+    if($val > 1120.00 && $val < 1299.99) return 53.20;
+    if($val > 1300.00 && $val < 1489.99) return 61.40;
+    if($val > 1490.00 && $val < 1709.99) return 70.40;
+    if($val > 1710.00 && $val < 1909.99) return 79.60;
+    if($val > 1910.00 && $val < 2139.99) return 89.10;
+    if($val > 2140.00 && $val < 2379.99) return 99.40;
+    if($val > 2380.00 && $val < 2629.99) return 110.20;
+    if($val > 2630.00 && $val < 2919.99) return 122.10;
+    if($val > 2920.00 && $val < 3137.99) return 133.30;
+    if($val > 3138.00) return 138.10;
+    return 0.0;
+}
+function getTotalContribution($val){
+    if($val > 200.00 && $val < 339.99) return 35.70;
+    if($val > 340.00 && $val < 449.99) return 52.20;
+    if($val > 450.00 && $val < 609.99) return 69.90;
+    if($val > 610.00 && $val < 759.99) return 90.30;
+    if($val > 760.00 && $val < 929.99) return 111.60;
+    if($val > 930.00 && $val < 1119.99) return 135.30;
+    if($val > 1120.00 && $val < 1299.99) return 159.60;
+    if($val > 1300.00 && $val < 1489.99) return 184.20;
+    if($val > 1490.00 && $val < 1709.99) return 211.20;
+    if($val > 1710.00 && $val < 1909.99) return 238.80;
+    if($val > 1910.00 && $val < 2139.99) return 267.30;
+    if($val > 2140.00 && $val < 2379.99) return 298.20;
+    if($val > 2380.00 && $val < 2629.99) return 330.60;
+    if($val > 2630.00 && $val < 2919.99) return 366.30;
+    if($val > 2920.00 && $val < 3137.99) return 399.90;
+    if($val > 3138.00) return 414.30;
+    return 0.0;
 }
--- 816000325/index.php
+++ 816000325/index.php
@@ -1,5 +1,13 @@
 <?php
 include 'functions.php';
+
+global $nisNum;
+global $salaries;
+global $totSalary;
+global $weeklyContributions;
+global $totWeekly;
+global $totalContributions;
+global $totTotal;
 ?>
 <!DOCTYPE html>
 <html>
@@ -18,12 +26,12 @@ include 'functions.php';
         <form action="index.php" method="post">
 
             <div class="form-group form-inline">
-                <label for="nisNum" style="font-size:20px; padding-right: 10px"><B>NIS Number</B></label>
+                <label for="nisNum" style="font-size:20px; padding-right: 10px"><B>Enter NIS Number</B></label>
                 <input type="number" class="form-control" id="nisNum" name="nisNum" placeholder="Enter Number">
             </div>
 
             <div class="form-horizontal">
-                <label for="nisNum" style="font-size:20px"><b>Salary</b></label>
+                <label for="nisNum" style="font-size:20px"><b>Enter Weekly Salary:</b></label>
 
                 <div class="row p-3">
                     <div class="col-xs-">
@@ -54,32 +62,78 @@ include 'functions.php';
         </form>
         <?php
         if(isset($_POST['nisNum'])){
-            initForm();
+            initForm(
+                $nisNum, $salaries,
+                $weeklyContributions, $totalContributions,
+                $totSalary, $totWeekly, $totTotal
+            );
+//            echo "Nis number: ". $nisNum;
+//            echo "<BR>";
+//            echo implode("<BR>", $salaries);
+//            echo "<BR>";
+//            echo $salaries[0];
         }
-        else echo "Please fill out form.<BR><BR><BR>";
+        else echo "Please fill out form.";
         ?>
-
+        <BR><BR><BR>
+        <div class="form-group form-inline">
+            <label for="nisNum" style="font-size:20px; padding-right: 10px"><B>NIS Number</B></label>
+            <input type="number" class="form-control" value="<?php echo $nisNum; ?>" placeholder="Enter Number" readonly>
+        </div>
+        <BR>
+        <label for="nisNum" style="font-size:20px"><b>Salary</b></label>
+        <div class="row p-3">
+            <div class="col-xs-">
+                <label for="week1">Week 1</label>
+                <input type="number" min="0.00" value="<?php echo $salaries[0]; ?>" class="form-control" placeholder="tot1 1" readonly>
+            </div>
+            <div class="col-xs-">
+                <label for="week1">Week 2</label>
+                <input type="number" min="0.00" value="<?php echo $salaries[1]; ?>" class="form-control" placeholder="tot1 2" readonly>
+            </div>
+            <div class="col-xs-">
+                <label for="week1">Week 3</label>
+                <input type="number" min="0.00" value="<?php echo $salaries[2]; ?>" class="form-control" placeholder="tot1 3" readonly>
+            </div>
+            <div class="col-xs-">
+                <label for="week1">Week 4</label>
+                <input type="number" min="0.00" value="<?php echo $salaries[3]; ?>" class="form-control" placeholder="tot1 4" readonly>
+            </div>
+            <div class="col-xs-">
+                <label for="week1">Week 5</label>
+                <input type="number" min="0.00" value="<?php echo $salaries[4]; ?>" class="form-control" placeholder="tot1 5" readonly>
+            </div>
+            <div class="col-xs-">
+                <label for="week1">Total</label>
+                <input type="number" min="0.00" value="<?php echo $totSalary; ?>" class="form-control" placeholder="tot1 5" readonly>
+            </div>
+        </div>
+        <BR><BR>
         <label for="nisNum" style="font-size:20px"><b>Employee NIS Contributions</b></label>
         <div class="row p-3">
             <div class="col-xs-">
                 <label for="week1">Week 1</label>
-                <input type="number" min="0.00" step="0.01" value="0.00" class="form-control" placeholder="econ1 1" name="econ1" readonly>
+                <input type="number" min="0.00" value="<?php echo $weeklyContributions[0]; ?>" class="form-control" placeholder="econ1 1" name="econ1" readonly>
             </div>
             <div class="col-xs-">
                 <label for="week1">Week 2</label>
-                <input type="number" min="0.00" step="0.01" value="0.00" class="form-control" placeholder="econ2 2" name="econ2" readonly>
+                <input type="number" min="0.00" value="<?php echo $weeklyContributions[1]; ?>" class="form-control" placeholder="econ2 2" name="econ2" readonly>
             </div>
             <div class="col-xs-">
                 <label for="week1">Week 3</label>
-                <input type="number" min="0.00" step="0.01" value="0.00" class="form-control" placeholder="econ3 3" name="econ3" readonly>
+                <input type="number" min="0.00" value="<?php echo $weeklyContributions[2]; ?>" class="form-control" placeholder="econ3 3" name="econ3" readonly>
             </div>
             <div class="col-xs-">
                 <label for="week1">Week 4</label>
-                <input type="number" min="0.00" step="0.01" value="0.00" class="form-control" placeholder="econ4 4" name="econ4" readonly>
+                <input type="number" min="0.00" value="<?php echo $weeklyContributions[3]; ?>" class="form-control" placeholder="econ4 4" name="econ4" readonly>
             </div>
             <div class="col-xs-">
                 <label for="week1">Week 5</label>
-                <input type="number" min="0.00" step="0.01" value="0.00" class="form-control" placeholder="econ5 5" name="econ5" readonly>
+                <input type="number" min="0.00" value="<?php echo $weeklyContributions[4]; ?>" class="form-control" placeholder="econ5 5" name="econ5" readonly>
+            </div>
+            <div class="col-xs-">
+                <label for="week1">Total</label>
+                <input type="number" min="0.00" value="<?php echo $totWeekly; ?>" class="form-control" placeholder="tot1 5" readonly>
             </div>
         </div>
         <BR><BR>
@@ -87,23 +141,27 @@ include 'functions.php';
         <div class="row p-3">
             <div class="col-xs-">
                 <label for="week1">Week 1</label>
-                <input type="number" min="0.00" step="0.01" value="0.00" class="form-control" placeholder="tot1 1" name="tot1" readonly>
+                <input type="number" min="0.00" value="<?php echo $totalContributions[0]; ?>" class="form-control" placeholder="tot1 1" readonly>
             </div>
             <div class="col-xs-">
                 <label for="week1">Week 2</label>
-                <input type="number" min="0.00" step="0.01" value="0.00" class="form-control" placeholder="tot1 2" name="tot2" readonly>
+                <input type="number" min="0.00" value="<?php echo $totalContributions[1]; ?>" class="form-control" placeholder="tot1 2" readonly>
             </div>
             <div class="col-xs-">
                 <label for="week1">Week 3</label>
-                <input type="number" min="0.00" step="0.01" value="0.00" class="form-control" placeholder="tot1 3" name="tot3" readonly>
+                <input type="number" min="0.00" value="<?php echo $totalContributions[2]; ?>" class="form-control" placeholder="tot1 3" readonly>
             </div>
             <div class="col-xs-">
                 <label for="week1">Week 4</label>
-                <input type="number" min="0.00" step="0.01" value="0.00" class="form-control" placeholder="tot1 4" name="tot4" readonly>
+                <input type="number" min="0.00" value="<?php echo $totalContributions[3]; ?>" class="form-control" placeholder="tot1 4" readonly>
             </div>
             <div class="col-xs-">
                 <label for="week1">Week 5</label>
-                <input type="number" min="0.00" step="0.01" value="0.00" class="form-control" placeholder="tot1 5" name="tot5" readonly>
+                <input type="number" min="0.00" value="<?php echo $totalContributions[4]; ?>" class="form-control" placeholder="tot1 5" readonly>
+            </div>
+            <div class="col-xs-">
+                <label for="week1">Total</label>
+                <input type="number" min="0.00" value="<?php echo $totTotal; ?>" class="form-control" placeholder="tot1 5" readonly>
             </div>
         </div>
 
